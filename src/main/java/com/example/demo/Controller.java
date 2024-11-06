542f01652ca213e91c2984d8c1002de34e05e1b6
reddydama <yogeswar073@gmail.com>
2024-11-06 19:05:50 +0530

    Changes done from QA Machine.

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
@@ -10,6 +11,12 @@ public class Controller {
 	@GetMapping("/")
 	public String getMethodName() {
 		return "Welcome to devops";
+	}
+	
+	@GetMapping("/userName")
+	public String  getName(@RequestParam String userName) {
+		
+		return "Welome to QA Team"+userName.toString();
 	}
 	
 
